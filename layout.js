66fb0d7187782c9ae044c7b42c91c00ec431ad08
lVeliarl <dimalitx@gmail.com>
2019-06-15 17:57:33 +0300

    refactor: code restructure

--- layout.js
+++ layout.js
@@ -1,6 +1,36 @@
 let current_year = new Date().getFullYear();
 const list_items = ['Dashboard', 'Users', 'Products', 'Locations'];
 
+const toolbar = {
+  view: "toolbar", css: "webix_dark",
+  cols: [
+    { view: "label", label: "My App", align: "center", maxWidth: "100" },
+    {},
+    {
+      view: "button", type: "icon", icon: "mdi mdi-account",
+      label: "Profile", autowidth: "true", css: "webix_transparent",
+      popup: "settings"
+    }
+  ]
+};
+
+const sidebar = {
+  rows: [
+    { view: "list", 
+      data: list_items, 
+      select: true, 
+      css: "list", 
+      width: 200,
+      on: {
+        onAfterSelect: function(id) {
+          $$(id).show();
+        }
+      }
+       },
+    { view: "label", label: "<span class='webix_icon wxi-check'></span>Connected", align: "center", css: "connect-message" }
+  ]
+};
+
 const small_film_set = [
   { id: 1, title: "The Shawshank Redemption", year: 1994, votes: 678790, rating: 9.2, rank: 1, category: "Thriller" },
   { id: 2, title: "The Godfather", year: 1972, votes: 511495, rating: 9.2, rank: 2, category: "Crime" },
@@ -10,94 +40,86 @@ const small_film_set = [
   { id: 6, title: "12 Angry Men", year: 1957, votes: 164558, rating: 8.9, rank: 6, category: "Western" }
 ];
 
-const form = [
-  { template: "Edit films", type: "section" },
-  { view: "text", label: "Title", name: "title", invalidMessage: "Title cannot be empty" },
-  { view: "text", label: "Year", name: "year", invalidMessage: `Year should be between 1970 and ${current_year}` },
-  { view: "text", label: "Rating", name: "rating", invalidMessage: "Rating cannot be empty or equal 0" },
-  { view: "text", label: "Votes", name: "votes", invalidMessage: "Votes must be less than 100000" },
-  {
-    cols: [
-      {
-        view: "button", value: "Add new", css: "webix_primary", click: function () {
-          let result = $$("edit_films").validate();
-          if (result) {
-            let current_entry = $$("edit_films").getValues();
-            $$("film_list").add(current_entry);
-            webix.message("Entry successfully added");
+const data = { view: "datatable", id: "film_list", data: small_film_set, autoConfig: true, gravity: 3 };
+
+const form = {
+  view: "form", id: "edit_films", 
+  elements: [
+    { template: "Edit films", type: "section" },
+    { view: "text", label: "Title", name: "title", invalidMessage: "Title cannot be empty" },
+    { view: "text", label: "Year", name: "year", invalidMessage: `Year should be between 1970 and ${current_year}` },
+    { view: "text", label: "Rating", name: "rating", invalidMessage: "Rating cannot be empty or equal 0" },
+    { view: "text", label: "Votes", name: "votes", invalidMessage: "Votes must be less than 100000" },
+    {
+      cols: [
+        {
+          view: "button", value: "Add new", css: "webix_primary", click: function () {
+            let result = $$("edit_films").validate();
+            if (result) {
+              let current_entry = $$("edit_films").getValues();
+              $$("film_list").add(current_entry);
+              webix.message("Entry successfully added");
+            }
+          }
+        },
+        {
+          view: "button", value: "Clear", click: function () {
+            webix.confirm({
+              title: "Clear the form",
+              text: "Do you really want to clear this form?"
+            }).then(
+              function () {
+                $$("edit_films").clear();
+                $$("edit_films").clearValidation();
+                webix.message("Form cleared");
+              },
+              function () {
+                webix.message("Cancelled");
+              }
+            );
           }
         }
-      },
-      {
-        view: "button", value: "Clear", click: function () {
-          webix.confirm({
-            title: "Clear the form",
-            text: "Do you really want to clear this form?"
-          }).then(
-            function () {
-              $$("edit_films").clear();
-              $$("edit_films").clearValidation();
-              webix.message("Form cleared");
-            },
-            function () {
-              webix.message("Cancelled");
-            }
-          );
-        }
-      }
-    ]
+      ]
+    },
+    {}
+  ],
+  rules: {
+    title: webix.rules.isNotEmpty,
+    votes: function (vote) {
+      return vote < 100000;
+    },
+    year: function (year) {
+      return year >= 1970 && year <= current_year;
+    },
+    rating: function (rating) {
+      return webix.rules.isNotEmpty(rating) && rating > 0;
+    },
   },
-  {}
-];
+  elementsConfig: {
+    bottomPadding: 30
+  },
+  gravity: 2
+};
 
 const copyright = "The software is provided by <a target='_blank' href='https://webix.com'>https://webix.com</a>. All rights reserved (c)";
 
+const footer = { view: "template", template: copyright, autoheight: true, css: "footer" };
+
+const main = {
+  cells: [
+    { id: "Dashboard", cols: [ data, form ]},
+    { id: "Users", template: "Users"},
+    { id: "Products", template: "Products"},
+    { id: "Admin", template: "Admin"}
+  ]
+}
+
 webix.ui({
+  id: "app",
   rows: [
-    {
-      view: "toolbar", css: "webix_dark",
-      cols: [
-        { view: "label", label: "My App", align: "center", maxWidth: "100" },
-        {},
-        {
-          view: "button", type: "icon", icon: "mdi mdi-account",
-          label: "Profile", autowidth: "true", css: "webix_transparent",
-          popup: "settings"
-        }
-      ]
-    },
-    {
-      cols: [
-        {
-          rows: [
-            { view: "list", data: list_items, css: "list" },
-            { view: "label", label: "<span class='webix_icon wxi-check'></span>Connected", align: "center", css: "connect-message" }
-          ]
-        },
-        { view: "resizer" },
-        { view: "datatable", id: "film_list", data: small_film_set, autoConfig: true, gravity: 3 },
-        {
-          view: "form", id: "edit_films", elements: form,
-          rules: {
-            title: webix.rules.isNotEmpty,
-            votes: function (vote) {
-              return vote < 100000;
-            },
-            year: function (year) {
-              return year >= 1970 && year <= current_year;
-            },
-            rating: function (rating) {
-              return webix.rules.isNotEmpty(rating) && rating > 0;
-            },
-          },
-          elementsConfig: {
-            bottomPadding: 30
-          },
-          gravity: 2
-        }
-      ]
-    },
-    { view: "template", template: copyright, autoheight: true, css: "footer" }
+    toolbar,
+    { cols: [ sidebar, { view: "resizer" }, main ]},
+    footer
   ]
 });
 
